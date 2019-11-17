ce9e511d798ce3f06287f11625eee2696952a50e
Daniel Hamilton <nightdelivery123@gmail.com>
2019-11-17 16:41:33 -0500

    added gravity

--- metroidvania-game/scenes/player/Player.gd
+++ metroidvania-game/scenes/player/Player.gd
@@ -1,25 +1,24 @@
 extends Node2D
 
 export var speed = 100
-
+const GRAVITY = 600
 
 func _ready():
 	pass # Replace with function body.
 
 func _process(delta):
     var animator = $KinematicBody2D/Sprites/PlayerAnimator
-    var velocity = Vector2()  # The player's movement vector.
+    var velocity = Vector2(0, GRAVITY)  # The player's movement vector.
     if Input.is_action_pressed("ui_right"):
-        velocity.x += 1
+        velocity.x += speed
     if Input.is_action_pressed("ui_left"):
-        velocity.x -= 1
+        velocity.x -= speed
     if Input.is_action_pressed("ui_down"):
         pass
     if Input.is_action_pressed("ui_up"):
         pass
-    if velocity.length() > 0:
-        velocity = velocity.normalized() * speed
+    if velocity.x != 0:
         animator.play("Character_Walking")
     else:
         animator.play("Idle")
-    velocity = $KinematicBody2D.move_and_slide(velocity, Vector2(0, -1))
+    velocity = $KinematicBody2D.move_and_slide(velocity)
