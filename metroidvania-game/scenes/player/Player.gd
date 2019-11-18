714a5073e549b9496bde201c948034df8a7f732a
Daniel Hamilton <nightdelivery123@gmail.com>
2019-11-18 18:17:54 -0500

    Lots of problems that we need to work though here. For starters, the tilemap is messed up. All the tiles it turns out need to be the same size in order to fit next to each other properly. This will likely take some time to get right. Argh

--- metroidvania-game/scenes/player/Player.gd
+++ metroidvania-game/scenes/player/Player.gd
@@ -1,24 +1,33 @@
 extends Node2D
 
 export var speed = 100
+export var jumpPower = 200
 const GRAVITY = 600
+var jumping = false
+var velocity = Vector2(0, 0)  # The player's movement vector.
 
 func _ready():
 	pass # Replace with function body.
 
 func _process(delta):
     var animator = $KinematicBody2D/Sprites/PlayerAnimator
-    var velocity = Vector2(0, GRAVITY)  # The player's movement vector.
+    var jump = Input.is_action_just_pressed("ui_up")
     if Input.is_action_pressed("ui_right"):
-        velocity.x += speed
-    if Input.is_action_pressed("ui_left"):
-        velocity.x -= speed
-    if Input.is_action_pressed("ui_down"):
-        pass
-    if Input.is_action_pressed("ui_up"):
-        pass
+        velocity.x = speed
+    elif Input.is_action_pressed("ui_left"):
+        velocity.x = speed * -1
+    elif !jumping:
+        velocity.x = 0
+        animator.play("Idle")
     if velocity.x != 0:
         animator.play("Character_Walking")
-    else:
-        animator.play("Idle")
-    velocity = $KinematicBody2D.move_and_slide(velocity)
+        $KinematicBody2D/Sprites/SpriteSheet.flip_h = velocity.x < 0
+        
+    if jump and $KinematicBody2D.is_on_floor():
+        jumping = true
+        velocity.y = jumpPower * -1
+    if jumping and $KinematicBody2D.is_on_floor():
+        jumping = false
+    
+    velocity.y += GRAVITY * delta
+    velocity = $KinematicBody2D.move_and_slide(velocity, Vector2(0, -1))
