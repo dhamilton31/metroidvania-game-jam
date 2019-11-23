6d654961fe87919d3544d43f201980316d264114
discoPiranha <57836112+discoPiranha@users.noreply.github.com>
2019-11-22 22:28:09 -0500

    Jumping
    - Added conditions to use jump up and down animations.
    - Fixed code indent level to work with Godot defaults
    - Position of Kinematic Body during jumping may need to be tweaked
    - I've yet to read about Git best practices, so I hope I don't bork anything!

--- metroidvania-game/scenes/player/Player.gd
+++ metroidvania-game/scenes/player/Player.gd
@@ -10,24 +10,31 @@ func _ready():
 	pass # Replace with function body.
 
 func _process(delta):
-    var animator = $KinematicBody2D/Sprites/PlayerAnimator
-    var jump = Input.is_action_just_pressed("ui_up")
-    if Input.is_action_pressed("ui_right"):
-        velocity.x = speed
-    elif Input.is_action_pressed("ui_left"):
-        velocity.x = speed * -1
-    elif !jumping:
-        velocity.x = 0
-        animator.play("Idle")
-    if velocity.x != 0:
-        animator.play("Character_Walking")
-        $KinematicBody2D/Sprites/SpriteSheet.flip_h = velocity.x < 0
-        
-    if jump and $KinematicBody2D.is_on_floor():
-        jumping = true
-        velocity.y = jumpPower * -1
-    if jumping and $KinematicBody2D.is_on_floor():
-        jumping = false
-    
-    velocity.y += GRAVITY * delta
-    velocity = $KinematicBody2D.move_and_slide(velocity, Vector2(0, -1))
+	var animator = $KinematicBody2D/Sprites/PlayerAnimator
+	var jump = Input.is_action_just_pressed("ui_up")
+	if Input.is_action_pressed("ui_right"):
+		velocity.x = speed
+	elif Input.is_action_pressed("ui_left"):
+		velocity.x = speed * -1
+	elif !jumping and $KinematicBody2D.is_on_floor():
+		velocity.x = 0
+		animator.play("Idle")
+
+	if velocity.y > 0:
+		animator.play("Character_jumping_up")
+	elif velocity.y < 0:
+		animator.play("Character_jumping_down")
+
+	elif velocity.x != 0:
+		#animator.play("Character_Walking")
+		animator.play("Character_Walking")
+		$KinematicBody2D/Sprites/SpriteSheet.flip_h = velocity.x < 0
+
+	if jump and $KinematicBody2D.is_on_floor():
+		jumping = true
+		velocity.y = jumpPower * -1
+
+	if jumping and $KinematicBody2D.is_on_floor():
+		jumping = false
+	velocity.y += GRAVITY * delta
+	velocity = $KinematicBody2D.move_and_slide(velocity, Vector2(0, -1))
